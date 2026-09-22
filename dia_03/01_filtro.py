47399119725faf539d8e71b213e2b9a9f1ff9045
Abner <abner.a.filho@gmail.com>
2026-09-21 21:00:09 -0300

    feat: dia 03 filtros

---
+++ dia_03/01_filtro.py
@@ -0,0 +1,68 @@
+# %%
+
+import pandas as pd
+
+pontos = [10, 1, 1, 1, 50, 100, 130, 30, 25, 50]
+filtro = []
+
+valores_50_mais = []
+for i in pontos:
+    filtro.append(i >= 50)
+
+resultado = []
+for i in range(len(pontos)):
+    if filtro[i]:
+        resultado.append(pontos[i])
+
+resultado
+filtro
+# %%
+
+brinquedo = pd.DataFrame(
+    {
+        "nome": ["teo", "nah", "mah"],
+        "idade": [32,35,14],
+        "uf": ["sp", "pr", "rj"],     
+     }
+)
+
+filtro = brinquedo["idade"] >= 18
+brinquedo[filtro]
+
+# %%
+
+df = pd.read_csv("../data/transacoes.csv", sep = ";")
+df.head()
+
+# %%
+
+filtro = df["QtdePontos"] >= 50
+df[filtro]
+
+# %%
+
+filtro = (df["QtdePontos"] >= 50 ) & (df["QtdePontos"] < 100)
+filtro
+df[filtro]
+
+# %%
+
+filtro = (df["QtdePontos"] == 1) | (df["QtdePontos"] == 100)
+df[filtro]
+
+# %%
+
+filtro = (df["QtdePontos"] > 0) & (df["QtdePontos"]<=50) | (df["DtCriacao"]>='2025-01-01')
+df[filtro]
+
+# %%
+
+True  and True  = True
+True  and False = False
+False and True  = False
+False and False = False
+
+True  or True  = True
+True  or False = True
+False or True  = True
+False or False = False
